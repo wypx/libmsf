07f1e05a6081ccd2d03b5ae7a441017c390f0eca
luotang <tang.luo@ucloud.cn>
2020-01-29 12:35:20 +0800

    rm zookeeper code

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import os
 
-def MSF_BUILD_SHOW():
+def DisplayGithub():
     print("\n")
     print("          Welcome to join me http://luotang.me                 ")
     print("          Welcome to join me https://github.com/wypx/libmsf    ")
+    print("          Welcome to join me https://github.com/wypx/mobile    ")
     print("\n")
 
-def MSF_BUILD_LOGO():
+def DisplayLogo():
     print("                       .::::.                                  ")
     print("                     .::::::::.                                ")
     print("                    :::::::::::                                ")
@@ -28,46 +30,44 @@ def MSF_BUILD_LOGO():
     print("    ```` ':.          ':::::::::'                  ::::..      ")
     print("                       '.:::::'                    ':'````..   ")
 
-def MSF_MKDIR_BUILD(build_path):
-    bDirExist = os.path.exists(build_path)
-    if not bDirExist:
-        os.makedirs(build_path) 
-        print("-- Dir \'" + build_path + '\' has been created successfully.')
+def MakeBuildDir(buildPath):
+    if not os.path.exists(buildPath):
+        os.makedirs(buildPath) 
+        print("-- Dir \'" + buildPath + '\' has been created successfully.')
     else:
-        print("-- Dir \'" + build_path + '\' has already exist now.')
+        print("-- Dir \'" + buildPath + '\' has already exist now.')
 
-def MSF_BUILD_LIBRARY(build_path):
-    os.chdir(build_path)
+def MakeBuildLib(buildPath):
+    os.chdir(buildPath)
     os.system("cmake ../../lib")
     os.system("make -j8")
     #os.system("make install")
 
-def MSF_BUILD_APPLICATION(build_path):
-    os.chdir(build_path)
+def MakeBuildApp(buildPath):
+    os.chdir(buildPath)
     os.system("cmake ../../app")
     os.system("make -j8")
     #os.system("make install")
 
-def MSF_BUILD_PROTOBUF(build_path):
-    os.chdir(build_path)
+def MakeBuildProto(buildPath):
+    os.chdir(buildPath)
     os.system("protoc -I=./ --cpp_out=./ *.proto")
     
-
-def MSF_BUILD_CLEAN(build_path):
+def MakeBuildClean(build_path):
     #os.removedirs(build_path)
     os.system("cd ..")
     os.system("rm -rvf build")
 
 if __name__ == '__main__':
-    MSF_BUILD_SHOW()
-    # MSF_BUILD_LOGO()
+    DisplayGithub()
+    # DisplayLogo()
     print("\n")
     print("\n******************* Micro Service Framework Build Starting **************************\n")
-    MSF_BUDILD_ROOT = os.getcwd()
-    MSF_MKDIR_BUILD("build/lib")
-    MSF_MKDIR_BUILD("build/app")
-    MSF_BUILD_LIBRARY(MSF_BUDILD_ROOT + "/build/lib")
-    MSF_BUILD_APPLICATION(MSF_BUDILD_ROOT + "/build/app")
-    # MSF_BUILD_CLEAN("build")
+    BuildRoot = os.getcwd()
+    MakeBuildDir("build/lib")
+    MakeBuildDir("build/app")
+    MakeBuildLib(BuildRoot + "/build/lib")
+    MakeBuildApp(BuildRoot + "/build/app")
+    # MakeBuildClean("build")
     print("\n******************* Micro Service Framework Build Ending ****************************\n")
 

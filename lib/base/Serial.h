faaf146bb7f76cfca081b0b9d7f80953b63e4650
luotang <tang.luo@ucloud.cn>
2020-08-30 14:52:37 +0800

    serial utils

--- lib/base/Serial.h
+++ lib/base/Serial.h
@@ -18,9 +18,40 @@
 
 namespace MSF {
 
+// Enumeration defines the possible bytesizes for the serial port.
+typedef enum {
+  FiveBits = 5,
+  SixBits = 6,
+  SevenBits = 7,
+  EightBits = 8
+} SerialByteSize;
+
+// Enumeration defines the possible flowcontrol types for the serial port.
+typedef enum {
+  FlowControlNone = 0,
+  FlowControlSoftware,
+  FlowControlHardware
+} SerialFlowControl;
+
+// Enumeration defines the possible parity types for the serial port.
+typedef enum {
+  ParityNone = 0,
+  ParityOdd = 1,
+  ParityEven = 2,
+  ParityMark = 3,
+  ParitySpace = 4
+} SerialParity;
+
+// Enumeration defines the possible stopbit types for the serial port.
+typedef enum {
+  StopBitsOne = 1,
+  StopBitsTwo = 2,
+  StopBitsOnePointFive
+} SerialStopBits;
+
 int32_t OpenSerialPort(const char *port);
 int SetSerialBaud(const int fd, const int speed);
-int SetSerialRawMode(const int fd);
+int SetSerialRawMode(const int fd, SerialFlowControl flowcontrol = FlowControlHardware);
 
 }  // namespace MSF
 #endif

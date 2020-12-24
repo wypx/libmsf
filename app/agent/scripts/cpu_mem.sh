3bdb4611c6fe17e910016f874e6041b836f28cf1
luotang <tang.luo@ucloud.cn>
2020-12-25 00:14:59 +0800

    udpate app

--- app/agent/scripts/cpu_mem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while sleep 1; do
-	DATE="$(date)";
-	PS="$( ps -p `pidof AgentServer` -o 'pcpu,pmem,rss,vsz')";
-	TOP="$( top -p `pidof AgentServer` -n1 -b -H -c )"
-	OUTPUT=$DATE"\n\n"$TOP"\n\nps\n"$PS"\n";
-	echo -e "$OUTPUT" | tee -a cpu-mem.log;
-done;
